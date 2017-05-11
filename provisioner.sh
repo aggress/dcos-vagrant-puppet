1ab803dc5dd1c23c0d683b4894dc585523c632b9
Richard Shaw <richard@aggress.net>
2017-05-11 08:58:33 +0100

    Moved shell provisioner to inline

--- provisioner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#install the Puppet repo and the agent
-yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum -y update
-yum -y install puppet-agent
-
-#run the main manifest
-/opt/puppetlabs/bin/puppet apply /vagrant/site.pp
